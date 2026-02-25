559e57b68e69ce369ca6fbab1b4d94481c7d6745
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-02-25 16:38:44 +0500

    start

---
+++ index.js
@@ -0,0 +1,52 @@
+
+// 1-exam
+
+// function createCounter(init = 5) {
+//     let count = init;
+  
+//     return {
+//       increment() {
+//         count++;
+//         return count;
+//       },
+  
+//       decrement() {
+//         count--;
+//         return count;
+//       },
+  
+//       reset() {
+//         count = init;
+//         return count;
+//       }
+//     };
+//   }
+
+
+//2-exam
+
+
+// var map = function(arr, fn) {
+//     let result = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         result[i] = fn(arr[i], i);
+//     }
+
+//     return result;
+// };
+
+
+//3-exam
+
+// var filter = function(arr, fn) {
+//     let result = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (Boolean(fn(arr[i], i))) {
+//             result.push(arr[i]);
+//         }
+//     }
+
+//     return result;
+// };
