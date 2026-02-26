734ba188bfb378a2b5b3aa551b96bf3a1ce25213
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-02-26 17:27:47 +0500

    exam

--- index.js
+++ index.js
@@ -109,3 +109,20 @@
 // };
 
 
+
+
+// example leetcode
+
+// async function sleep(millis) {
+//   return new Promise((resolve) => {
+//     setTimeout(resolve, millis);
+//   });
+// }
+
+// async function demo() {
+//   console.log("Start");
+//   await sleep(2000); 
+//   console.log("End after 2 seconds");
+// }
+
+// demo();
