f082dffa1f63e10e034d1ce1dfa6c03d810df553
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-02-26 13:24:17 +0500

    add

--- index.js
+++ index.js
@@ -83,3 +83,21 @@
 // }
 
 // once()
+
+
+function memoize(fn) {
+  const cache = {};
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+
+    if (key in cache) {
+      return cache[key];
+    }
+
+    const result = fn(...args);
+    cache[key] = result;
+
+    return result;
+  };
+}
