783657a6886958d301271d94564af43b8032f1dc
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-02-27 22:01:34 +0500

    promise functions

--- index.js
+++ index.js
@@ -236,3 +236,30 @@
 //         }, t);
 //     };
 // };
+
+
+// leetcode 
+
+var promiseAll = function(functions) {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    let completed = 0;
+    if (functions.length === 0) {
+      resolve([]);
+      return;
+    }
+    functions.forEach((fn, index) => {
+      fn()
+        .then((value) => {
+          results[index] = value;
+          completed++;
+          if (completed === functions.length) {
+            resolve(results);
+          }
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  });
+};
