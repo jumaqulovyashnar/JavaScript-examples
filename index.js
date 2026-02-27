bd3dd8f415aa866b77f28ae33e6ce5f9b3ea12db
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-02-27 21:51:22 +0500

    new add

--- index.js
+++ index.js
@@ -155,3 +155,27 @@
 
 //   return cancelFn;
 // };
+
+
+
+// exams
+
+// let  timeLimit = function(fn, t) {
+//   return async function(...args) {
+//     return new Promise((resolve, reject) => {
+//       const timer = setTimeout(() => {
+//         reject("Time Limit Exceeded");
+//       }, t);
+//       fn(...args)
+//         .then((res) => {
+//           clearTimeout(timer);
+//           resolve(res);
+//         })
+//         .catch((err) => {
+//           clearTimeout(timer);
+//           reject(err);
+//         });
+
+//     });
+//   };
+// };
