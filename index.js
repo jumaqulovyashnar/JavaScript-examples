64253dff7e559145362b26e8f90c3b47b96d4122
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-02-27 21:46:29 +0500

    night

--- index.js
+++ index.js
@@ -126,3 +126,17 @@
 // }
 
 // demo();
+
+
+//  leetcode 
+
+var cancellable = function(fn, args, t) {
+  const timerId = setTimeout(() => {
+    fn(...args);
+  }, t);
+  const cancelFn = function() {
+    clearTimeout(timerId);
+  };
+
+  return cancelFn;
+};
