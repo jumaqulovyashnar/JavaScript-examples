a5831f9c5e3582eed4657831a59af95e5e98601b
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-02-27 21:46:54 +0500

    add

--- index.js
+++ index.js
@@ -130,13 +130,14 @@
 
 //  leetcode 
 
-var cancellable = function(fn, args, t) {
-  const timerId = setTimeout(() => {
-    fn(...args);
-  }, t);
-  const cancelFn = function() {
-    clearTimeout(timerId);
-  };
+// var cancellable = function(fn, args, t) {
+//   const timerId = setTimeout(() => {
+//     fn(...args);
+//   }, t);
+//   const cancelFn = function() {
+//     clearTimeout(timerId);
+//   };
 
-  return cancelFn;
-};
+//   return cancelFn;
+// };
+
