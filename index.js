97ecc43d7c418e4a800b7c557febf6c58001ecbe
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-02-27 21:48:39 +0500

    today

--- index.js
+++ index.js
@@ -141,3 +141,17 @@
 //   return cancelFn;
 // };
 
+
+// leetcode exam 
+
+// let cancellable = function(fn, args, t) {
+//   fn(...args);
+//   const intervalId = setInterval(() => {
+//     fn(...args);
+//   }, t);
+//   const cancelFn = function() {
+//     clearInterval(intervalId);
+//   };
+
+//   return cancelFn;
+// };
