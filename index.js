832528d9bbe17013531830f95077126602c0651e
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-03-03 13:48:33 +0500

    add

--- index.js
+++ index.js
@@ -320,16 +320,16 @@
 
 // leetcode exam
 
-Array.prototype.groupBy = function(fn) {
-  const result = {};
+// Array.prototype.groupBy = function(fn) {
+//   const result = {};
 
-  for (let i = 0; i < this.length; i++) {
-    const key = fn(this[i]);
-    if (!result[key]) {
-      result[key] = [];
-    }
-    result[key].push(this[i]);
-  }
+//   for (let i = 0; i < this.length; i++) {
+//     const key = fn(this[i]);
+//     if (!result[key]) {
+//       result[key] = [];
+//     }
+//     result[key].push(this[i]);
+//   }
 
-  return result;
-};
+//   return result;
+// };
