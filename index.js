8645a4010d8bd914ad226460abf9d29045a00cab
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-03-03 16:40:31 +0500

    add

--- index.js
+++ index.js
@@ -342,3 +342,26 @@
 //     return fn(a) - fn(b);
 //   });
 // };
+
+
+
+// leetcode  exam 
+
+
+let join = function (arr1, arr2) {
+    const map = new Map();
+
+    for (let obj of arr1) {
+        map.set(obj.id, { ...obj });
+    }
+
+    for (let obj of arr2) {
+        if (map.has(obj.id)) {
+            map.set(obj.id, { ...map.get(obj.id), ...obj });
+        } else {
+            map.set(obj.id, { ...obj });
+        }
+    }
+
+    return Array.from(map.values()).sort((a, b) => a.id - b.id);
+};
