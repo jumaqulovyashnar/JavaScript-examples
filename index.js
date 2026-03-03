04e61a9a0cef6963317a39eb6a63a5674a31df13
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-03-03 17:07:44 +0500

    add

--- index.js
+++ index.js
@@ -370,8 +370,8 @@
 // map methods example 
 
 
-let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-let b = a.map((i) => {
-    return  i**2;
-})
-console.log(b);
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let b = a.map((i) => {
+//     return  i**2;
+// })
+// console.log(b);
