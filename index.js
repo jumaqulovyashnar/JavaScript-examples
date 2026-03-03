a0f3900288a692cda6b1ca6620e327b4ea7bcfcb
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-03-03 16:40:58 +0500

    add last

--- index.js
+++ index.js
@@ -348,20 +348,20 @@
 // leetcode  exam 
 
 
-let join = function (arr1, arr2) {
-    const map = new Map();
+// let join = function (arr1, arr2) {
+//     const map = new Map();
 
-    for (let obj of arr1) {
-        map.set(obj.id, { ...obj });
-    }
+//     for (let obj of arr1) {
+//         map.set(obj.id, { ...obj });
+//     }
 
-    for (let obj of arr2) {
-        if (map.has(obj.id)) {
-            map.set(obj.id, { ...map.get(obj.id), ...obj });
-        } else {
-            map.set(obj.id, { ...obj });
-        }
-    }
+//     for (let obj of arr2) {
+//         if (map.has(obj.id)) {
+//             map.set(obj.id, { ...map.get(obj.id), ...obj });
+//         } else {
+//             map.set(obj.id, { ...obj });
+//         }
+//     }
 
-    return Array.from(map.values()).sort((a, b) => a.id - b.id);
-};
+//     return Array.from(map.values()).sort((a, b) => a.id - b.id);
+// };
