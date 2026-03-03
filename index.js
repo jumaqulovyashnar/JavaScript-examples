581de235e0ab9ffbec5b739f176bd08296bf45c9
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-03-03 17:07:30 +0500

    add map methods

--- index.js
+++ index.js
@@ -365,3 +365,13 @@
 
 //     return Array.from(map.values()).sort((a, b) => a.id - b.id);
 // };
+
+
+// map methods example 
+
+
+let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let b = a.map((i) => {
+    return  i**2;
+})
+console.log(b);
