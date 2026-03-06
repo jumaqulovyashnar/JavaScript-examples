41790a24503c4b99dd0283facad11be968307a88
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-03-06 20:50:57 +0500

    second exam

--- index.js
+++ index.js
@@ -505,3 +505,21 @@
 //     return results;
 //   }
 // }
+
+
+
+// next exam 
+
+class ArrayWrapper {
+  constructor(nums) {
+    this.nums = nums;
+  }
+
+  valueOf() {
+    return this.nums.reduce((sum, n) => sum + n, 0);
+  }
+
+  toString() {
+    return `[${this.nums.join(",")}]`;
+  }
+}
