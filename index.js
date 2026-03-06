22ac60ea7cb9e569dbee781ec2843e327298c056
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-03-06 20:46:38 +0500

    add

--- index.js
+++ index.js
@@ -467,3 +467,41 @@
 //     return obj;
 //   }
 // }
+
+
+// leetcode exam
+
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  subscribe(eventName, callback) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+
+    this.events[eventName].push(callback);
+
+    return {
+      unsubscribe: () => {
+        const index = this.events[eventName].indexOf(callback);
+        if (index !== -1) {
+          this.events[eventName].splice(index, 1);
+        }
+      }
+    };
+  }
+
+  emit(eventName, args = []) {
+    if (!this.events[eventName]) return [];
+
+    const results = [];
+
+    for (const cb of this.events[eventName]) {
+      results.push(cb(...args));
+    }
+
+    return results;
+  }
+}
