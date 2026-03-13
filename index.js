8d1869d7cd1d674768059ec3257c0535fd567c2c
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-03-13 21:59:54 +0500

    add

--- index.js
+++ index.js
@@ -606,3 +606,23 @@
 // }
 
 // console.log(constructMaximumBinaryTree([1,2,3,6,0,4]));
+
+
+
+
+// let inorderTraversal = function(root) {
+//     let result = []
+
+//     function dfs(node){
+//         if(!node) return
+
+//         dfs(node.left)
+//         result.push(node.val)
+//         dfs(node.right)
+//     }
+
+//     dfs(root)
+
+//     return result
+// };
+
