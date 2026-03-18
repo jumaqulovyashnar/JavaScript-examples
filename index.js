351a4a4ad433c1bf2be7d5ff476c40d87b8a57ab
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-03-18 22:54:18 +0500

    add

--- index.js
+++ index.js
@@ -681,3 +681,25 @@
     
 //     return count;
 // };
+
+
+var eraseOverlapIntervals = function(intervals) {
+    if (intervals.length === 0) return 0;
+
+    intervals.sort((a, b) => a[1] - b[1]);
+
+    let count = 0;
+    let prevEnd = intervals[0][1];
+
+    for (let i = 1; i < intervals.length; i++) {
+        let [start, end] = intervals[i];
+
+        if (start < prevEnd) {
+            count++;
+        } else {
+            prevEnd = end;
+        }
+    }
+
+    return count;
+};
