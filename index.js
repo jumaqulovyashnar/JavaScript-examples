9fbb38bbf9137a125bc6411e5bd0aab7a4d0612e
jumaqulovyashnar <jumaqulovyashnar@gmail.com>
2026-07-02 13:14:24 +0500

    add

---
+++ index.js
@@ -0,0 +1,32 @@
+//Sizga n soni beriladi.1dan n gacha sonlarni chiqaring.Lekin:3 ga bo'linsa →"Fizz" 5 ga bo'linsa →"Buzz" Ham 3 ga ham 5 ga bo'linsa →"FizzBuzz" Aks holda sonning o'zi.
+
+
+
+
+// let dev='yashnar developer'
+
+
+// let charcode=dev.charCodeAt(10  )
+// console.table(charcode)
+
+
+
+// let word='A'
+
+// let words=word.charCodeAt(0)
+
+// let result=String.fromCharCode(words+1)
+// console.log(result);
+
+
+
+// for(let i=5; i<=15; i++){
+//     console.log(i);
+// }
+
+
+let names='yashnar'
+
+for(let i=0 ; i<names.length; i++){
+    console.log(i+1,names[i]);
+}
